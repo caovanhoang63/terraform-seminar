873304e4c102f998021ecd18c6bc30b2fbabaa5b
hoangcao <22520457@gm.uit.edu.vn>
2024-10-18 23:09:33 +0700

    change out put

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-# output "db_password" {
-#   value = module.database.config.password
-#   sensitive = true
-# }
-#
-# output "lb_dns_name" {
-#   value = module.autoscaling.lb_dns
-# }
+output "db_password" {
+  value = module.database.config.password
+  sensitive = true
+}
+
+output "lb_dns_name" {
+  value = module.autoscaling.lb_dns
+}
 
 output "s3_static_url" {
   value = module.static-store.s3_dns_name
